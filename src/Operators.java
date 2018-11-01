acced3f2734425815d5db95811a26a8c78cfe233
mjwaweru007 <mjwaweru007@gmail.com>
2018-11-01 16:32:06 +0300

    Java Variables

---
+++ src/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+    public static void main(String[] args) {
+        int x=5;
+        int y=7;
+        int z=8;
+        int h=9;
+        --z;
+        ++h;
+        System.out.println(x+y );
+        System.out.println(x-y );
+        System.out.println(x*y );
+        System.out.println(y%x );
+        System.out.println(z);
+        System.out.println(h);
+
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x;
+        x=12;
+        String name;
+        name= "jose";
+
+        float decimal;
+        decimal=5.655f;
+
+        char letters;
+        letters= 'A';
+
+        boolean t;
+        t=true;
+
+        System.out.println(name + System.lineSeparator() + x + System.lineSeparator() + t );
+        System.out.println(decimal);
+
+    }
+}
+
